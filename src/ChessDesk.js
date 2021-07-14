3ea9a6f0f645876b9bc6f1d43996e439195329b3
vyacheslavkostyanets <vyacheslav.kostyanets@gmail.com>
2021-07-14 23:20:51 +0300

    add Coordinate

--- src/ChessDesk.js
+++ src/ChessDesk.js
@@ -1,186 +1,50 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
 import { Rectangle } from './Rectangle'
-import { BlackBishop } from './figures/blackbishop'
-import { Blacking } from './figures/blackking'
-import { BlackRook } from './figures/blackrook'
-import { BlackHorse } from './figures/blackhorse'
-import { BlackQueen } from './figures/blackqueen'
-import { BlackPawns } from './figures/blackpawns'
-import { WhitePawns } from './figures/whitepawns'
-import { WhiteKing } from './figures/whiteking'
-import { WhiteQueen } from './figures/whitequeen'
-import { WhiteBishop } from './figures/whitebishop'
-import { WhiteHorse } from './figures/whitehorse'
-import { WhiteRook } from './figures/whiterook'
-import { WhiteHorseSymbol } from './figures/whiteHorseSymbol'
-
+import { BlackBishop } from './figures/blackBishop'
+import { BlacKing } from './figures/blackKing'
+import { BlackPawn } from './figures/blackPawn'
+import { WhitePawn } from './figures/whitePawn'
+import { BlackRook } from './figures/blackRook'
+import { BlackKnight } from './figures/blackKnight'
+import { BlackQueen } from './figures/blackQueen'
+import { WhiteKing } from './figures/whiteKing'
+import { WhiteBishop } from './figures/whiteBishop'
+import { WhiteRook } from './figures/whiteRook'
+import { WhiteKnight } from './figures/whiteKnight'
+import { WhiteQueen } from './figures/whiteQueen'
+import { WhiteKnightSymbol } from './symbolPieces/whiteKnightSymbol'
+import { WhiteRookSymbol } from './symbolPieces/whiteRookSymbol'
+import { WhiteQueenSymbol } from './symbolPieces/whiteQueenSymbol'
+import { BlackBishopSymbol } from './symbolPieces/blackBishopSymbol'
+import { BlacKingSymbol } from './symbolPieces/blackKingSymbol'
+import { BlackRookSymbol } from './symbolPieces/blackRookSymbol'
+import { BlackKnightSymbol } from './symbolPieces/blackKnightSymbol'
+import { BlackQueenSymbol } from './symbolPieces/blackQueenSymbol'
+import { BlackPawnSymbol } from './symbolPieces/blackPawnSymbol'
+import { WhitePawnSymbol } from './symbolPieces/whitePawnSymbol'
+import { WhiteKingSymbol } from './symbolPieces/whiteKingSymbol'
+import { WhiteBishopSymbol } from './symbolPieces/whiteBishopSymbol'
 
 function ChessDesk() {
-    const [PiecesLocation, setPiecesLocation] = useState([]);
-    const [darkPieces, setdarkPieces] = useState([]);
-    const [whitePieces, setwhitePieces] = useState([]);
-
-
     const board = [];
     const pieces = [];
     const verticalCoordinates = ['1', '2', '3', '4', '5', '6', '7', '8'];
     const horizontalCoordinates = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-    // verticalCoordinates.forEach((elV, index) => {
-    //     pieces.push([]);
-    //     horizontalCoordinates.forEach((elH, i) => {
-    //         let isPieceDark = false;
-    //         let isPieceWhite = false;
-    //         let PieceDark = undefined;
-    //         let PieceWhite = undefined;
-    //         let locationDarkPiece = elV + elH;
-    //         let locationWhitePiece = elV + elH;
-    //         switch (locationWhitePiece) {
-    //             case '1a':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhiteRook />
-    //                 break;
-    //             case '1h':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhiteRook />
-    //                 break;
-    //             case '1b':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhiteHorse />
-    //                 break;
-    //             case '1g':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhiteHorse />
-    //                 break;
-    //             case '1c':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhiteBishop />
-    //                 break;
-    //             case '1f':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhiteBishop />
-    //                 break;
-    //             case '1d':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhiteQueen />
-    //                 break;
-    //             case '1e':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhiteKing />
-    //                 break;
-    //             case '2a':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhitePawns />
-    //                 break;
-    //             case '2h':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhitePawns />
-    //                 break;
-    //             case '2b':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhitePawns />
-    //                 break;
-    //             case '2g':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhitePawns />
-    //                 break;
-    //             case '2c':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhitePawns />
-    //                 break;
-    //             case '2f':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhitePawns />
-    //                 break;
-    //             case '2d':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhitePawns />
-    //                 break;
-    //             case '2e':
-    //                 isPieceWhite = true
-    //                 PieceWhite = <WhitePawns />
-    //                 break;
-    //             default: isPieceWhite = false
-    //         }
-    //         switch (locationDarkPiece) {
-    //             case '8a':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackRook />
-    //                 break;
-    //             case '8b':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackHorse />
-    //                 break;
-    //             case '8c':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackBishop />
-    //                 break;
-    //             case '8d':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackQueen />
-    //                 break;
-    //             case '8e':
-    //                 isPieceDark = true
-    //                 PieceDark = <Blacking />
-    //                 break;
-    //             case '8f':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackBishop />
-    //                 break;
-    //             case '8g':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackHorse />
-    //                 break;
-    //             case '8h':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackRook />
-    //                 break;
-    //             case '7a':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackPawns />
-    //                 break;
-    //             case '7b':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackPawns />
-    //                 break;
-    //             case '7c':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackPawns />
-    //                 break;
-    //             case '7d':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackPawns />
-    //                 break;
-    //             case '7e':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackPawns />
-    //                 break;
-    //             case '7f':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackPawns />
-    //                 break;
-    //             case '7g':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackPawns />
-    //                 break;
-    //             case '7h':
-    //                 isPieceDark = true
-    //                 PieceDark = <BlackPawns />
-    //                 break;
-    //             default: isPieceDark = false
-    //         }
+    const whitePawns = [];
+    const blackPawns = [];
 
-    //         pieces[index].push({
-    //             isPieceDark: isPieceDark,
-    //             isPieceWhite: isPieceWhite,
-    //             PieceDark: PieceDark,
-    //             PieceWhite: PieceWhite,
-    //             x: elH,
-    //             y: elV,
-    //         })
-    //     }
-    //     )
-    // });
+    for (let i = 0; i < 8; i++) {
+        // whitePawns.push(<Pawn side="black" x={i * 100} y="600" width="100" height="100" />)
+        // whitePawns.push(<Pawn side="white" x={i * 100} y="600" width="100" height="100" />)
+        whitePawns.push(<WhitePawn x={i * 100} y="600" width="100" height="100" />)
+    }
+    for (let i = 0; i < 8; i++) {
+        blackPawns.push(<BlackPawn x={i * 100} y="100" width="100" height="100" />)
+    }
+
+
+
     console.log(pieces, 'pieces')
     for (let v = 0; v < verticalCoordinates.length; v++) {
         for (let h = 0; h < horizontalCoordinates.length; h++) {
@@ -211,23 +75,55 @@ function ChessDesk() {
     console.log(board, 'board')
 
     return <svg width="800" height="800" version="1.1">
-        <WhiteHorseSymbol />
-        {board}
-        <WhiteHorse x="150" y="250" width="100" height="100" />
-        {/* <Blacking />
-            <BlackBishop />
+        <WhiteQueenSymbol />
+        <WhiteKnightSymbol />
+        <WhiteRookSymbol />
+        <WhiteBishopSymbol />
+        <WhiteKingSymbol />
+        <WhitePawnSymbol />
+        <BlackBishopSymbol />
+        <BlackKnightSymbol />
+        <BlackRookSymbol />
+        <BlacKingSymbol />
+        <BlackPawnSymbol />
+        <BlackQueenSymbol />
 
-            <BlackRook />
-            <BlackHorse />
-            <BlackQueen />
-            <BlackPawns />
-            <WhiteBishop />
-            <WhitePawns />
-            <WhiteKing />
-            <WhiteQueen />
-            <WhiteHorse />
-            <WhiteRook /> */}
-        {/* <WhiteRook /> */}
+        {board}
+        <BlackBishop x="200" y="0" width="100" height="100" />
+        <BlackBishop x="500" y="0" width="100" height="100" />
+
+        <WhiteKnight x="100" y="700" width="100" height="100" />
+        <WhiteKnight x="600" y="700" width="100" height="100" />
+        <WhiteRook x="0" y="700" width="100" height="100" />
+        <WhiteRook x="700" y="700" width="100" height="100" />
+        <WhiteBishop x="200" y="700" width="100" height="100" />
+        <WhiteBishop x="500" y="700" width="100" height="100" />
+        <WhiteKing x="300" y="700" width="100" height="100" />
+        <WhiteQueen x="400" y="700" width="100" height="100" />
+        {/* <WhitePawn x="0" y="600" width="100" height="100" />
+        <WhitePawn x="100" y="600" width="100" height="100" />
+        <WhitePawn x="200" y="600" width="100" height="100" />
+        <WhitePawn x="300" y="600" width="100" height="100" />
+        <WhitePawn x="400" y="600" width="100" height="100" />
+        <WhitePawn x="500" y="600" width="100" height="100" />
+        <WhitePawn x="600" y="600" width="100" height="100" />
+        <WhitePawn x="700" y="600" width="100" height="100" /> */}
+        {whitePawns}
+        <BlackKnight x="100" y="0" width="100" height="100" />
+        <BlackKnight x="600" y="0" width="100" height="100" />
+        <BlackRook x="0" y="0" width="100" height="100" />
+        <BlackRook x="700" y="0" width="100" height="100" />
+        <BlacKing x="300" y="0" width="100" height="100" />
+        {/* <BlackPawn x="0" y="100" width="100" height="100" />
+        <BlackPawn x="100" y="100" width="100" height="100" />
+        <BlackPawn x="200" y="100" width="100" height="100" />
+        <BlackPawn x="300" y="100" width="100" height="100" />
+        <BlackPawn x="400" y="100" width="100" height="100" />
+        <BlackPawn x="500" y="100" width="100" height="100" />
+        <BlackPawn x="600" y="100" width="100" height="100" />
+        <BlackPawn x="700" y="100" width="100" height="100" /> */}
+        {blackPawns}
+        <BlackQueen x="400" y="0" width="100" height="100" />
     </svg >
 }
 
--- src/figures/whitehorse.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function WhiteHorse(props) {
-    return <use href="#white-horse" {...props}></use>
-}
-export { WhiteHorse }
-
-
