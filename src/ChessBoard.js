6d1d0e5ab56b4968d615281a620422d53b06b50c
vyacheslavkostyanets <vyacheslav.kostyanets@gmail.com>
2021-08-03 23:19:22 +0300

    Realize Use State. Knight starts to move

--- src/ChessBoard.js
+++ src/ChessBoard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 import './App.css';
 import { Rectangle } from './Rectangle'
@@ -19,21 +19,37 @@ import { Knight } from './figures/knight'
 // import { Pawn } from './figures/pawn'
 
 function ChessBoard() {
+  const myRef = useRef()
   const background = [];
+
+  const [position, setPosition] = useState({
+    cellX: 0,
+    cellY: 0,
+    startX: 0,
+    startY: 0,
+    x: 600,
+    y: 0,
+    mouseDown: false
+  })
+
   const [side, setSide] = useState('black')
 
-  const [currentX, setCurrentX] = useState(600)
-  const [currentY, setCurrentY] = useState(0)
+  // const [currentX, setCurrentX] = useState(600)
+  // const [currentY, setCurrentY] = useState(0)
 
-  const [startX, setStartX] = useState(0)
-  const [startY, setStartY] = useState(0)
+  // const [startX, setStartX] = useState(0)
+  // const [startY, setStartY] = useState(0)
 
-  const [x, setX] = useState(currentX)
-  const [y, setY] = useState(currentY)
+  // const [x, setX] = useState(currentX)
+  // const [y, setY] = useState(currentY)
 
-  const [mouseDown, setMouseDown] = useState(false)
+  // const [newPositionX, setNewPositionX] = useState(0)
+  // const [newPositionY, setNewPositionY] = useState(0)
 
-  const verticalCoordinates = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+  // const [mouseDown, setMouseDown] = useState(false)
+
+  const verticalCoordinates = ['8', '7', '6', '5', '4', '3', '2', '1'];
   const horizontalCoordinates = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
   for (let v = 0; v < verticalCoordinates.length; v++) {
@@ -52,43 +68,51 @@ function ChessBoard() {
     }
   }
 
-  return <svg width="800" height="800" version="1.1" onMouseMove={(e) => {
-    if (mouseDown) {
-      setX(currentX + e.clientX - startX)
-      setY(currentY + e.clientY - startY)
+  return <svg ref={myRef} width="800" height="800" version="1.1"
+    onMouseMove={(e) => {
+      const { x, y } = myRef.current.getBoundingClientRect()
+
+      if (position.mouseDown) {
+        setPosition((prevState => {
+          return {
+            ...prevState, startX: e.clientX, startY: e.clientY, mouseDown: true,
+            x: position.x + e.clientX - position.startX,
+            y: position.y + e.clientY - position.startY,
+            cellX: Math.floor(((e.clientX - x) / 100)),
+            cellY: Math.floor(((e.clientY - y) / 100)),
+          };
+        }))
+      }
     }
-  }}
+    }
   >
     <PieceSymbols></PieceSymbols>
 
     {background}
 
-    <Knight side={side}
+    <Knight side={side} key="8g"
+
 
       onMouseDown={(e) => {
-        setMouseDown(true)
-
-        setStartX(e.clientX);
-        setStartY(e.clientY);
-
-        console.log(e);
+        setPosition((prevState => {
+          return { ...prevState, startX: e.clientX, startY: e.clientY, mouseDown: true };
+        }))
       }}
 
       onMouseUp={(e) => {
-        setMouseDown(false)
 
-        let cx = currentX + (e.clientX - startX) // 523
-        let cy = currentY + (e.clientY - startY) // 423
+        setPosition((prevState => {
+          return {
+            ...prevState,
+            x: position.cellX * 100,
+            y: position.cellY * 100,
+            mouseDown: false
+          };
+        }))
 
-        setCurrentX(cx - (cx % 100))
-        setCurrentY(cy - (cy % 100))
 
-        setX(x - (cx % 100))
-        setY(y - (cy % 100))
-
-        console.log(e);
       }}
-      x={x} y={y} width="100" height="100" />
+      x={position.x} y={position.y} width="100" height="100" />
   </svg >
 }
 
